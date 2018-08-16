8d4768199bb3cd5dc54324a41f5b638bcf48c593
Serzh <sky@iMac-Serzh.local>
2018-08-16 22:57:34 +0300

    First commit

---
+++ ci_test.go
@@ -0,0 +1,21 @@
+package main
+
+import "testing"
+
+func TestCI(t *testing.T) {
+	a := 2
+	b := 3
+	c := a + b
+	if c != 5 {
+		panic("wrong c result")
+	}
+}
+
+func TestCISecond(t *testing.T) {
+	a := 2
+	b := 3
+	c := a * b
+	if c != 6 {
+		panic("wrong c result")
+	}
+}
